73cf2493a69edd97ed030335a80d2bc7ba1e3922
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-05-21 22:43:20 +1000

    New model and controller added

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult AllMovies()
+        {
+            var movie = new Movie() { Name = "Kung Fu Panda" };
+
+            return View(movie);
+        }
+    }
+}
