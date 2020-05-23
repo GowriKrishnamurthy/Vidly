d1dc9c4ebc0ef309619da4a8c35d38fa6de92d67
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-05-23 16:14:05 +1000

    WebApi Helper methds

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -26,27 +26,27 @@ namespace Vidly.Controllers.Api
         }
 
         //GET /api/customers/1
-        public CustomerDto GetCustomer(int id)
+        public IHttpActionResult GetCustomer(int id)
         {
             var customer = _applicationDbContext.Customers.SingleOrDefault(c=>c.Id==id);
 
             if (customer == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
+                return NotFound();
 
-            return Mapper.Map<Customer,CustomerDto>(customer);
+            return Ok(Mapper.Map<Customer,CustomerDto>(customer));
         }
 
         //POST /api/customers
         [HttpPost]
-        public CustomerDto CreateCustomer(CustomerDto customerDto)
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
+                return BadRequest();
             var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
             _applicationDbContext.Customers.Add(customer);
             _applicationDbContext.SaveChanges();
             customerDto.Id = customer.Id;
-            return customerDto;
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id),customerDto);
         }
 
         //PUT /api/customers/1
--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -19,7 +19,7 @@ namespace Vidly.Dtos
 
         public byte MembershipTypeId { get; set; }
 
-        [MemberAgeLimit]
+        // [MemberAgeLimit]
         public DateTime? Birthdate { get; set; }
     }
 }
