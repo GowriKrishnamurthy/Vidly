d4c3bc659fcd55e28ed25226bbaba7f0c3a19976
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-05-23 14:18:38 +1000

    applying Anti forgery token

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -53,6 +53,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
             if (!ModelState.IsValid)
